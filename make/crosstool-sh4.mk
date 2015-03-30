31f75f15fced7209190a5a64fca23d4117acd095
max10 <max_10@gmx.de>
2015-03-30 18:56:48 +0200

    del STLINUX ISO

--- make/crosstool-sh4.mk
+++ make/crosstool-sh4.mk
@@ -1,21 +1,5 @@
 # makefile to build crosstool
 
-## stlinux crosstool is pretty lame since we only install the RPMs, but why bother
-## building everything for yourself if they provide a working, tested, supported
-## toolchain etc?
-
-# the mount point for the ISOs
-$(STLINUX_SRC_MNT) $(STLINUX_SH4_MNT):
-	mkdir -p $@
-
-# mount the ISOs. later, we'll just download what's needed on demand, but for
-# now this is less work.
-stlinux-isomount: | $(STLINUX_SRC_MNT) $(STLINUX_SH4_MNT)
-	test -e $(STLINUX_SRC_MNT)/stlinux24-host-filesystem-1.0-7.src.rpm || \
-		sudo mount -oloop,ro $(STLINUX_SRC_ISO) $(STLINUX_SRC_MNT)
-	test -e $(STLINUX_SH4_MNT)/README || \
-		sudo mount -oloop,ro $(STLINUX_SH4_ISO) $(STLINUX_SH4_MNT)
-
 crosstool-rpminstall: \
 $(STL_ARCHIVE)/stlinux24-cross-sh4-binutils-$(BINUTILS_VER).i386.rpm \
 $(STL_ARCHIVE)/stlinux24-cross-sh4-binutils-dev-$(BINUTILS_VER).i386.rpm \
